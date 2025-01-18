77f921afd63c6ff56594b403f56d5dafabf22419
brobert <brobert@valiha.com>
2025-01-18 08:05:52 +0100

    .

--- Labs/Azure/lab1/IaC/main.tf
+++ Labs/Azure/lab1/IaC/main.tf
@@ -1,66 +1,99 @@
+# Resource Group
 resource "azurerm_resource_group" "lab1" {
   name     = "lab1-resources"
   location = "East US"
 }
 
-resource "azurerm_virtual_machine" "lab1" {
-  name                  = "lab1-vm"
-  location              = azurerm_resource_group.lab1.location
-  resource_group_name   = azurerm_resource_group.lab1.name
-  vm_size               = "Standard_B2s"
-
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  }
-
-  storage_os_disk {
-    name              = "lab1-os-disk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-
-  os_profile {
-    computer_name  = "lab1-vm"
-    admin_username = "azureuser"
-    admin_password = "P@ssw0rd123!"
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-
-  network_interface_ids = [
-    azurerm_network_interface.lab1.id,
-  ]
+# Virtual Network
+resource "azurerm_virtual_network" "lab1_vnet" {
+  name                = "lab1-vnet"
+  location            = azurerm_resource_group.lab1.location
+  resource_group_name = azurerm_resource_group.lab1.name
+  address_space       = ["10.0.0.0/16"]
 }
 
-resource "azurerm_network_interface" "lab1" {
+# Subnet
+resource "azurerm_subnet" "lab1_subnet" {
+  name                 = "lab1-subnet"
+  resource_group_name  = azurerm_resource_group.lab1.name
+  virtual_network_name = azurerm_virtual_network.lab1_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Public IP
+resource "azurerm_public_ip" "lab1_public_ip" {
+  name                = "lab1-public-ip"
+  location            = azurerm_resource_group.lab1.location
+  resource_group_name = azurerm_resource_group.lab1.name
+  allocation_method   = "Dynamic"
+}
+
+# Network Interface
+resource "azurerm_network_interface" "lab1_nic" {
   name                = "lab1-nic"
   location            = azurerm_resource_group.lab1.location
   resource_group_name = azurerm_resource_group.lab1.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.lab1.id
+    subnet_id                     = azurerm_subnet.lab1_subnet.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.lab1_public_ip.id
   }
 }
 
-resource "azurerm_virtual_network" "lab1" {
-  name                = "lab1-vnet"
-  address_space       = ["10.0.0.0/16"]
+# Virtual Machine
+resource "azurerm_linux_virtual_machine" "lab1_vm" {
+  name                = "lab1-vm"
   location            = azurerm_resource_group.lab1.location
   resource_group_name = azurerm_resource_group.lab1.name
+  size                = "Standard_B1ls" # Free tier or low-cost instance
+  admin_username      = "azureuser"
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("~/.ssh/robert@eurecom.fr.pub") # Path to your SSH public key
+  }
+
+  network_interface_ids = [
+    azurerm_network_interface.lab1_nic.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
 }
 
-resource "azurerm_subnet" "lab1" {
-  name                 = "lab1-subnet"
-  resource_group_name  = azurerm_resource_group.lab1.name
-  virtual_network_name = azurerm_virtual_network.lab1.name
-  address_prefixes     = ["10.0.2.0/24"]
+# Security Group
+resource "azurerm_network_security_group" "lab1_nsg" {
+  name                = "lab1-nsg"
+  location            = azurerm_resource_group.lab1.location
+  resource_group_name = azurerm_resource_group.lab1.name
+
+  security_rule {
+    name                       = "AllowHTTP"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Associate NSG with Subnet
+resource "azurerm_subnet_network_security_group_association" "lab1_subnet_nsg" {
+  subnet_id                 = azurerm_subnet.lab1_subnet.id
+  network_security_group_id = azurerm_network_security_group.lab1_nsg.id
 }
 
