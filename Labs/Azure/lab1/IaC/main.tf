9589b2c99dabd175eb8bf3135f9a482a4851552d
setrar <brobert@valiha.com>
2025-01-18 08:51:25 +0100

    Update main.tf

--- Labs/Azure/lab1/IaC/main.tf
+++ Labs/Azure/lab1/IaC/main.tf
@@ -93,6 +93,18 @@ resource "azurerm_network_security_group" "lab1_nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 1000
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 # Associate NSG with Subnet
