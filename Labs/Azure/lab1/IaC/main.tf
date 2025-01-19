cd646bfda1ed1d4e394a9dc0f283ad2c0df41ec3
brobert <brobert@valiha.com>
2025-01-19 07:36:42 +0100

    .

--- Labs/Azure/lab1/IaC/main.tf
+++ Labs/Azure/lab1/IaC/main.tf
@@ -206,3 +206,19 @@ output "static_site_url" {
   value = azurerm_storage_account.lab1-sa.primary_web_endpoint
 }
 
+#### -----------------------
+##   ACR Configuration
+#### -----------------------
+
+resource "azurerm_container_registry" "lab1-acr" {
+  name                = "acrclouds2025eurbr"
+  resource_group_name = azurerm_resource_group.lab1.name
+  location            = azurerm_resource_group.lab1.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+output "acr_login_server" {
+  value = azurerm_container_registry.lab1-acr.login_server
+}
+
